7e92f9bb7b50ed42826b73ee8392f577053510de
Nikol061 <nikolkoleva48@gmail.com>
2025-03-17 13:10:26 +0200

    super admin

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ public class Program
             options.UseSqlServer(connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<ApplicationDbContext>();
         builder.Services.AddControllersWithViews();
 
